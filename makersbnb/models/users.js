df4d665cb822822162bdaf7b92a8c19ebeb66576
Thady Condon <thadycondon@hotmail.com>
2016-08-09 12:27:33 +0100

    Added sequelize and connected to database.

---
+++ makersbnb/models/users.js
@@ -0,0 +1,27 @@
+// var Sequelize = require('sequelize')
+//  , sequelize = new Sequelize('makersbnb_dev', 'thadycondon', 'password', {
+//      dialect: "postgres", // or 'sqlite', 'postgres', 'mariadb'
+//      port:    5432// or 5432 (for postgres)
+//    });
+//
+// var User = sequelize.define('User', {
+//  username: Sequelize.STRING,
+//  email: Sequelize.STRING,
+//  password: Sequelize.STRING
+// });
+//
+//  var user = User.create({
+//     username: 'Joe',
+//     email: 'email@email.com',
+//     password: '12345'
+//   });
+//
+//   module.exports = User;
+
+  // User.create({
+  //  username: 'john-doe',
+  //  email: 'email@email.com',
+  //  password: 'i-am-so-great'
+  // }).then(function(user) {
+  //  /* ... */
+  // })
